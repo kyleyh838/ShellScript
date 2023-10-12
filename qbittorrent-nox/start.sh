69ebd8ee4612e819770f5d8d2af89075bf2751a4
kyleyh838 <64256385+kyleyh838@users.noreply.github.com>
2023-10-12 09:33:01 +0800

    Update start.sh

--- qbittorrent-nox/start.sh
+++ qbittorrent-nox/start.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
 clear
 echo "欢迎使用qbittorrent-nox 4.3.9一键安装脚本"
 echo "请确认已切换root用户执行脚本"
 echo "一键脚本合集GitHub开源地址：https://github.com/kyleyh838/ShellScript"
 
-if [ $(id -u) -eq 0 ]; then
-    sudo apt update -y && sudo apt upgrade -y
-    sudo apt-get install software-properties-common -y
-    sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
-    sudo apt update -y
-    sudo apt install qbittorrent-nox=1:4.3.9.99~202110311443-7435-01519b5e7~ubuntu20.04.1 -y
-    file_to_edit="/etc/systemd/system/qbittorrent-nox.service"
-    echo "[Unit]
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Error：${plain} 当前不是root用户，无法执行脚本，请切换root用户后重新执行！\n" && exit 1
+
+echo "${yellow}正在安装qbittorrent-nox，请稍等。。。${plain}"
+apt update -y && apt upgrade -y
+apt-get install software-properties-common -y
+add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+apt update -y
+apt install qbittorrent-nox=1:4.3.9.99~202110311443-7435-01519b5e7~ubuntu20.04.1 -y
+file_to_edit="/etc/systemd/system/qbittorrent-nox.service"
+echo "[Unit]
 Description=qBittorrent Command Line Client
 After=network.target
 
@@ -26,16 +34,11 @@ Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target" > "$file_to_edit"
-    systemctl daemon-reload
-    systemctl enable qbittorrent-nox
-    systemctl start qbittorrent-nox
+systemctl daemon-reload
+systemctl enable qbittorrent-nox
+systemctl start qbittorrent-nox
 
-    clear
-    echo "恭喜，qbittorrent-nox 4.3.9安装成功！"
-    echo "Web-UI访问地址http://ip:8088，默认用户名密码 admin adminadmin"
-
-else
-    echo 
-    echo "Error:当前不是root用户，无法执行脚本，请切换root用户后重新执行。"
-    exit 1
-fi
+clear
+echo "恭喜，qbittorrent-nox 4.3.9安装成功！"
+echo -e "Web-UI访问地址http://ip:8088\n默认用户名密码：admin adminadmin"
+exit 0
