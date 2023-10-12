144f57a5cb250d748463190fc3eb1a089370b3c9
kyleyh838 <64256385+kyleyh838@users.noreply.github.com>
2023-10-12 09:46:47 +0800

    Update start.sh

--- Docker/start.sh
+++ Docker/start.sh
@@ -13,37 +13,38 @@ echo "一键脚本合集GitHub开源地址：https://github.com/kyleyh838/ShellS
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Error：${plain} 当前不是root用户，无法执行脚本，请切换root用户后重新执行！\n" && exit 1
 
-    sudo apt update -y && sudo apt install wget -y
+clear
+echo "请选择需要执行的操作："
+echo "${green}1.安装Docker${plain}"
+echo "${green}2.安装Docker-compose${plain}"
+echo "${green}0.退出脚本${plain}"
+
+read choice
+
+case $choice in
+  1)
+    echo "${yellow}即将安装Docker，请稍等。。。${plain}"
+    apt update -y && apt install wget -y
+    wget -qO- get.docker.com | bash
+    systemctl enable docker
+    systemctl start docker
     clear
-    echo "请选择需要执行的操作："
-    echo "${green}1.安装Docker${plain}"
-    echo "${green}2.安装Docker-compose${plain}"
-    echo "${green}0.退出脚本${plain}"
-
-    read choice
-
-    case $choice in
-      1)
-        echo "${yellow}即将安装Docker，请稍等。。。${plain}"
-        wget -qO- get.docker.com | bash
-        systemctl enable docker
-        systemctl start docker
-        clear
-        echo "恭喜安装成功"
-        docker -v
-        exit 0
-        ;;
-      2)
-        echo "${yellow}即将安装Docker-compose，请稍等。。。${plain}"
-        sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
-        clear
-        echo "恭喜安装成功"
-        docker-compose --version
-        exit 0
-        ;;
-      *)
-        echo "已退出脚本，期待你的再次使用"
-        exit 0
-        ;;
-    esac
+    echo "恭喜安装成功"
+    docker -v
+    exit 0
+  ;;
+  2)
+    echo "${yellow}即将安装Docker-compose，请稍等。。。${plain}"
+    apt update -y
+    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    clear
+    echo "恭喜安装成功"
+    docker-compose --version
+    exit 0
+  ;;
+  *)
+    echo "已退出脚本，期待你的再次使用"
+    exit 0
+  ;;
+esac
